134854927b82516c908725e3613c7ae1f3b215af
Taras Bobriienko <taras3189@gmail.com>
2023-05-05 14:14:01 +0300

    HomeScreen 02 commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key, required this.errMsg}) : super(key: key);
   final String errMsg;
-  static const String _title = "Home";
+  static const String _title = 'Home';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: StatefulHomeWidget(errMsg: errMsg),
+      home: StatefulHomeWidget(errMsg: errMsg,),
     );
   }
 }
@@ -20,13 +21,12 @@ class StatefulHomeWidget extends StatefulWidget {
   final String errMsg;
 
   @override
-  State<StatefulHomeWidget> createState() =>
-      _StatefulHomeWidget(errMsg: errMsg);
+  // ignore: no_logic_in_create_state
+  State<StatefulHomeWidget> createState() => _StatefulHomeWidget(errMsg: errMsg);
 }
 
 class _StatefulHomeWidget extends State<StatefulHomeWidget> {
   _StatefulHomeWidget({required this.errMsg});
-
   final String errMsg;
   final storage = const FlutterSecureStorage();
 
@@ -45,6 +45,161 @@ class _StatefulHomeWidget extends State<StatefulHomeWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 252, 142, 54),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Color.fromARGB(255, 26, 255, 1)),
+        title: const Text(
+          'Flutter Auth Role',
+          style: TextStyle(
+            height: 1.171875,
+            fontSize: 18.0,
+            fontFamily: 'Roboto Condensed',
+            fontWeight: FontWeight.w500,
+            color: Color.fromARGB(255, 26, 255, 1),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: const Color.fromARGB(255, 252, 142, 54),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                ),
+                child: Center(
+                  child: Text(
+                    'MENU',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      height: 1.171875,
+                      fontSize: 18.0,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 26, 255, 1),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(width: 6.0, color: Color.fromARGB(255, 0, 0, 0)),
+                    bottom: BorderSide(width: 1.0, color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                ),
+                child: ListTile(
+                  title: const Text(
+                    'Users',
+                    style: TextStyle(
+                      height: 1.171875,
+                      fontSize: 18.0,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 26, 255, 1),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                        SlideRightRoute(page: const UsersScreen(errMsg: '',)));
+                  },
+                ),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(width: 6.0, color: Color.fromARGB(255, 0, 0, 0)),
+                    bottom: BorderSide(width: 1.0, color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                ),
+                child: ListTile(
+                  title: const Text(
+                    'Roles',
+                    style: TextStyle(
+                      height: 1.171875,
+                      fontSize: 18.0,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 26, 255, 1),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                        SlideRightRoute(page: const RolesScreen(errMsg: '',)));
+                  },
+                ),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(width: 6.0, color: Color.fromARGB(255, 0, 0, 0)),
+                    bottom: BorderSide(width: 1.0, color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                ),
+                child: ListTile(
+                  title: const Text(
+                    'Products',
+                    style: TextStyle(
+                      height: 1.171875,
+                      fontSize: 18.0,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 26, 255, 1),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                        SlideRightRoute(page: const ProductsScreen(errMsg: '',)));
+                  },
+                ),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(width: 6.0, color: Color.fromARGB(255, 0, 0, 0)),
+                    bottom: BorderSide(width: 1.0, color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                ),
+                child: ListTile(
+                  title: const Text(
+                    'Logout',
+                    style: TextStyle(
+                      height: 1.171875,
+                      fontSize: 18.0,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 26, 255, 1),
+                    ),
+                  ),
+                  onTap: () async {
+                    await storage.deleteAll();
+                    Navigator.push(context,
+                        SlideRightRoute(page: const LoginScreen(errMsg: 'User logged out',)));
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome Home!\nThis is the Role Based Authentication\nwith Permissions Apps',
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.visible,
+          style: TextStyle(
+            height: 1.171875,
+            fontSize: 18.0,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w500,
+            color: Color.fromARGB(255, 0, 0, 0),
+          ),
+        ),
+      ),
+    );
   }
 }
