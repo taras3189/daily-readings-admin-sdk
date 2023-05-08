c8a7dc6db49bf905f59b1c9d51c35d2ab860443c
Taras Bobriienko <taras3189@gmail.com>
2023-05-08 16:03:21 +0300

    CRUD 02 commit

--- lib/screens/users_screen.dart
+++ lib/screens/users_screen.dart
@@ -154,3 +154,60 @@ class _StatefulUsersWidget extends State<StatefulUsersWidget> {
     );
   }
 }
+
+class UserListWidget extends StatelessWidget {
+  final List<Users> users;
+
+  const UserListWidget({Key? key, required this.users}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: users.isEmpty ? 0 : users.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+          child: Card(
+            color: const Color.fromARGB(255, 252, 142, 54),
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(
+                  color: Color.fromARGB(255, 0, 0, 0), width: 1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          UserDetailsScreen(users: users[index])),
+                );
+              },
+              child: ListTile(
+                leading: const Icon(
+                  Icons.person,
+                  size: 48,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                ),
+                title: Text(
+                  users[index].fullname.toString(),
+                  style: const TextStyle(
+                    fontFamily: 'Roboto',
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+                subtitle: Text(
+                  users[index].email.toString(),
+                  style: const TextStyle(
+                    fontFamily: 'Roboto',
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
